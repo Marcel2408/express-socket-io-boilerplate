802484264332792b79cd539356181458d33425e1
elijah <elijah_burke@hotmail.co.uk>
2020-11-23 08:07:09 +0000

    feat: templating for install

--- bin/createBoilerplate.js
+++ bin/createBoilerplate.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const process = require('process');
-const createBoilerplate = require('../index.js');
+const createBoilerplate = require('../source/index.js');
 
 // check passed arguments
 const args = process.argv.splice(process.execArgv.length + 2);

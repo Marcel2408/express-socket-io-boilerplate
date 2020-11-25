6da864069435edd493314721a1c02c788fb66344
elijah <elijah_burke@hotmail.co.uk>
2020-11-25 08:07:07 +0000

    slight refactor to public index.js

--- test-framework/public/index.js
+++ test-framework/public/index.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('#form');
-const input = document.querySelector('#chat-message');
 const socketCount = document.querySelector('#socket-count');
 const clientId = document.querySelector('#socket-id');
 const homeLink = document.querySelector('#home-section-link');
@@ -11,57 +10,55 @@ const socket = io('http://localhost:3000');
 socket.emit('/root/new_socket_connected');
 let socketId;
 
-// Socket event handlers
-function addMessage(data) {
+const addMessage = (data) => {
   const { message, sender } = data;
   const text = document.createTextNode(message);
   const listItem = document.createElement('li');
-  if (sender === 'user') listItem.classList.add('list-item', 'right');
-  else listItem.classList.add('list-item', 'left');
+  listItem.append(text);
   const chat = document.getElementById('chat-list');
-  listItem.appendChild(text);
-  if (sender === 'user') setTimeout(() => chat.appendChild(listItem), 200);
-  else setTimeout(() => chat.appendChild(listItem), 1000);
-}
+  if (sender === 'user') {
+    listItem.classList.add('list-item', 'right');
+    setTimeout(() => chat.appendChild(listItem), 200);
+  } else {
+    listItem.classList.add('list-item', 'left');
+    setTimeout(() => chat.appendChild(listItem), 1000);
+  }
+};
 
-function getPrevMsgs({ messages, id }) {
+const getPrevMsgs = ({ messages, id }) => {
   messages.forEach((msg) => {
     const { message } = msg;
     addMessage({ message, sender: 'server' });
   });
   socketId = id;
   clientId.innerHTML = socketId;
-}
+};
 
-function updateSocketCount(data) {
-  const { clientCount } = data;
-  socketCount.innerHTML = clientCount;
-}
+const updateSocketCount = ({ clientCount }) => { socketCount.innerHTML = clientCount; };
 
-// Sends a chat message to the server
-function sendMessage(e) {
+const sendMessage = (e) => {
   e.preventDefault();
-  const { value } = input;
-  input.value = '';
+  const { value } = e.target.firstElementChild;
+  e.target.firstElementChild.value = '';
   socket.emit('/root/new_message', { socketId, message: value });
-}
+};
 
 // Helper functions
-function goToHomepage(e) {
+const goToHomepage = (e) => {
   e.preventDefault();
   homeSection.style.width = '100vw';
   homeSection.style.opacity = '1';
   docsSection.style.width = '0';
   docsSection.style.opacity = '0';
-}
+};
 
-function goToDocumentation(e) {
+const goToDocumentation = (e) => {
   e.preventDefault();
   docsSection.style.width = '100vw';
   docsSection.style.opacity = '1';
   homeSection.style.width = '0';
   homeSection.style.opacity = '0';
-}
+};
 
 // Reactive elements > Event listeners
 form.addEventListener('submit', sendMessage);
